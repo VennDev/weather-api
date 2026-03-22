be1759c06e12c26b241220b38e9d60ce2c5a53aa
VennDev <1050080275@sv.hcmunre.edu.vn>
2026-03-22 22:37:11 +0700

    Clean code

--- src/vennv/weather_api/routers/PublicRoutes.php
+++ src/vennv/weather_api/routers/PublicRoutes.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace vennv\weather_api\routers;
 
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use vosaka\http\router\Router;
 use vennv\weather_api\handlers\HealthHandler;
 use vennv\weather_api\handlers\WeatherHandler;
@@ -14,8 +12,8 @@ use vosaka\http\router\RouteGroup;
 final class PublicRoutes {
 	public static function new(Router &$router): void {
 		$router->group('/api/v1', function (RouteGroup $router): void {
-			$router->get('/health', fn(ServerRequestInterface $request): ResponseInterface => HealthHandler::health($request));
-			$router->get('/weather', fn(ServerRequestInterface $request): ResponseInterface => WeatherHandler::weather($request));
+			$router->get('/health', HealthHandler::health(...));
+			$router->get('/weather', WeatherHandler::weather(...));
 		});
 	}
 }
